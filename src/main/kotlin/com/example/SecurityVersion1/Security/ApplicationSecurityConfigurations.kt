f9f71066a13c2a76857b1660d938f439fd8ebae7
zubair-pc\zubair <szubair@gmail.com>
2021-05-06 19:50:41 +0500

    1. Now instead of database user we will do in memory user managment using userDetailsService and UserDetailManager 2. If our implimenting it we do login we will get error of password encoder

--- src/main/kotlin/com/example/SecurityVersion1/Security/ApplicationSecurityConfigurations.kt
+++ src/main/kotlin/com/example/SecurityVersion1/Security/ApplicationSecurityConfigurations.kt
@@ -1,9 +1,15 @@
 package com.example.SecurityVersion1.Security
 
+import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
+import org.springframework.security.core.userdetails.User
+import org.springframework.security.core.userdetails.UserDetailsService
+import org.springframework.security.crypto.factory.PasswordEncoderFactories
+import org.springframework.security.provisioning.InMemoryUserDetailsManager
+
 
 @Configuration
 @EnableWebSecurity
@@ -17,5 +23,19 @@ class ApplicationSecurityConfigurations :WebSecurityConfigurerAdapter(){
                 ?.authenticated()
                 ?.and()
                 ?.httpBasic()
+    }
+
+    @Bean
+    override fun userDetailsService(): UserDetailsService {
+
+
+
+        var annSmithUser = User.withUsername("annaSmith")
+                .password("password").roles("STUDENT").build()
+
+        val userDetailsManager = InMemoryUserDetailsManager()
+        userDetailsManager.createUser(annSmithUser)
+
+        return userDetailsManager
     }
 }
